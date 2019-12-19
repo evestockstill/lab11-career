2f98d3acb0bf805c782fb5fd206b271f27c6916e
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-18 17:23:58 -0800

    done

--- seed.js
+++ seed.js
@@ -2,14 +2,10 @@
 /* eslint-disable no-console */
 require('dotenv').config();
 require('./lib/utils/connect')();
-const colors = require('colors');
-
 
 const mongoose = require('mongoose');
 const { seedData } = require('./lib/utils/seed-data');
 
-
 seedData()
   .then(() => console.log('done'.green))
   .finally(() => mongoose.connection.close());
-  
